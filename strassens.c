56a3998b55b77fa1337a62738e6794af1a8ab0e8
alexsolis31 <solis@college.harvard.edu>
2016-03-21 23:36:48 -0400

    debugging matrix upload for Matrix A

--- strassens.c
+++ strassens.c
@@ -4,80 +4,80 @@
 #include <time.h>
 #include "strassens.h"
 
-// int readFile(char* fileName);
+int readFile(char* fileName, int dimension);
 
 int main(int argc, char **argv){
 
-	// parse inputs 
-	if(argc != 4){
-		printf("Command line input should be in the form  ./strassen 0 dimension inputfile" );
-		return -1; 
-	}
+  // parse inputs 
+  if(argc != 4){
+    printf("Command line input should be in the form  ./strassen 0 dimension inputfile" );
+    return -1; 
+  }
 
 
-	int flag = strtoul(argv[1],NULL, 10);
-	int dimension = strtoul(argv[2], NULL, 10);
-	char* fileName = argv[3];
-	readFile(fileName, dimension);
+  int flag = strtoul(argv[1],NULL, 10);
+  int dimension = strtoul(argv[2], NULL, 10);
+  char* fileName = argv[3];
+  readFile(fileName, dimension);
 
 
-   	return 0;
+    return 0;
  
 }
 
 
 int readFile(char* fileName, int dimension){
 
-	// reading in data from the inputFile 
-	char ch;
-	int ch1;
-	char* line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	FILE *fp = fopen(fileName, "r");
-	int d = dimension; 
+  // reading in data from the inputFile 
+  char ch;
+  int ch1;
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  FILE *fp = fopen(fileName, "r");
+  int d = dimension; 
 
-	if( fp == NULL ){
+  if( fp == NULL ){
       perror("Error while opening the file.\n");
       exit(EXIT_FAILURE);
    }
 
     printf("The contents of %s file are :\n", fileName);
 
-    int** matrixA = malloc(sizeof(int*) * d);
+    int holdingArray[2*d*d];
+    int counter = 0;
+
+    while((read = getline(&line, &len, fp)) != -1){
+      ch1 = atoi(line);
+      holdingArray[counter] = ch1; 
+      printf("%d \n", ch1);
+      counter++;
+    }
+
+    // print the holdingArray
+    for (int i = 0; i < (2*d*d); i++){
+      printf("%d", holdingArray[i]);
+    }
+
+    printf("\n");
+
+    int** matrixA = malloc(sizeof(int*)*d*d);
+
+    int countvar = 0;
+    for (int i = 0; i < d; i++){
+      for (int j = 0; j < d; j++){
+        printf("A: %d\n", matrixA[i][j]);
+        // printf("countvar: %d\n", countvar);
+        // printf("B: %d\n", holdingArray[countvar]);
+        // matrixA[i][j] = holdingArray[countvar];
+        countvar++;
+       }
+    }
+
     int** matrixB = malloc(sizeof(int*) * d);
-
-    for(int i = 0; i < d; i++){
-    	for (int j = 0; j < d; j++){
-	    	if((read = getline(&line, &len, fp)) != -1){
-				matrixA[i][j] = atoi(line);
-			}
-		}
-    }
-
-    for(int i = 0; i < d; i++){
-    	for (int j = 0; j < d; j++){
-	    	if((read = getline(&line, &len, fp)) != -1){
-				matrixB[i][j] = atoi(line);
-			}
-		}
-    }
-    return 0;
-
-
-    // while((read = getline(&line, &len, fp)) != -1){   	
-    // 	ch1 = atoi(line);
-    // 	for(int i = 0; i < d; i++){
-    // 		matrixA[i]
-    // 	}
-
-    // 	printf("%d \n", ch1);
-    // }
-
-    
-
-
-   	fclose(fp);
+   
+    fclose(fp);
+    return 0; 
 
 
 }
