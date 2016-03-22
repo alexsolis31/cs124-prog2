3d6df30a198512036384d0b691e6433c3c1c3d08
alexsolis31 <solis@college.harvard.edu>
2016-03-21 20:56:10 -0400

    Beginning

--- strassens.c
+++ strassens.c
@@ -1,1 +1,21 @@
-##
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "strassens.h"
+
+int main(int argc, char **argv){
+
+	// parse inputs 
+	if(argc != 4){
+		printf("Command line input shold be in the form  ./strassen 0 dimension inputfile" );
+		return -1; 
+	}
+
+	unsigned flag = strtoul(argv[1],NULL, 10);
+	unsigned dimension = strtoul(argv[2], NULL, 10);
+
+
+
+}
+
